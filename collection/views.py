f454ee71714a2e0072b5f562267975aee4e27d87
robert <stewarrm04@gmail.com>
2016-03-15 13:44:48 -0500

    Added query_sets to views and revised templates

--- collection/views.py
+++ collection/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
+from .models import Thing
 
 def index(request):
-    number = 6
-    thing = "thing name"
-    return render(request, 'index.html', {'number':number, 'thing':thing})
+    things = Thing.objects.all().order_by('name')
+    return render(request, 'index.html', {'things':things,})
