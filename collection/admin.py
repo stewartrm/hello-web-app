685659eb28dd2ed1ebd7b27fe0da9b4b65b10d12
robert <stewarrm04@gmail.com>
2016-03-15 13:28:45 -0500

    Added model and admin

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Thing
+
+class ThingAdmin(admin.ModelAdmin):
+    model = Thing
+    list_display = ('name', 'description',)
+    prepopulated_fields = {'slug': ('name',)}
+    
+admin.site.register(Thing, ThingAdmin)
