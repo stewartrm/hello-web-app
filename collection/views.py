11aef2593dd71c7c943a883ae6e812523df2821b
robert <stewarrm04@gmail.com>
2016-03-15 14:43:32 -0500

    Added detail page

--- collection/views.py
+++ collection/views.py
@@ -4,3 +4,7 @@ from .models import Thing
 def index(request):
     things = Thing.objects.all().order_by('name')
     return render(request, 'index.html', {'things':things,})
+    
+def thing_detail(request, slug):
+    thing = Thing.objects.get(slug=slug)
+    return render(request, 'things/thing_detail.html', {'thing':thing,})
