b3aa678bc90c24e76c3c4dbc7f588971cb1ef708
robert <stewarrm04@gmail.com>
2016-03-17 00:21:56 -0500

    added registration of one item per user with restricted editing

--- collection/views.py
+++ collection/views.py
@@ -1,43 +1,91 @@
-from django.shortcuts import render, redirect
-from django.template.defaultfilters import slugify
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
-from .forms import ThingForm
-from .models import Thing
+from django.shortcuts import render, redirect
+from django.template.defaultfilters import slugify
+
+from collection.forms import ThingForm
+from collection.models import Thing
+
+
 
 def index(request):
-    things = Thing.objects.all().order_by('name')
-    return render(request, 'index.html', {'things':things,})
-    
-def thing_detail(request, slug):
-    thing = Thing.objects.get(slug=slug)
-    return render(request, 'things/thing_detail.html', {'thing':thing,})
+    things = Thing.objects.all()
+    return render(request, 'index.html', {
+        'things': things,
+    })
 
-@login_required    
-def edit_thing(request, slug):
+
+def thing_detail(request, slug):
+    # grab the object...
     thing = Thing.objects.get(slug=slug)
+
+    # and pass to the template
+    return render(request, 'things/thing_detail.html', {
+        'thing': thing,
+    })
+
+
+@login_required
+def edit_thing(request, slug):
+    # grab the object...
+    thing = Thing.objects.get(slug=slug)
+
+    # grab the current logged in user and make sure they're the owner of the thing
     if thing.user != request.user:
         raise Http404
+
+    # set the form we're using...
     form_class = ThingForm
+
+    # if we're coming to this view from a submitted form,  
     if request.method == 'POST':
+        # grab the data from the submitted form
         form = form_class(data=request.POST, instance=thing)
+
         if form.is_valid():
+            # save the new data
             form.save()
             return redirect('thing_detail', slug=thing.slug)
+
+    # otherwise just create the form
     else:
         form = form_class(instance=thing)
-    return render(request, 'things/edit_thing.html', {'thing':thing, 'form':form})
-    
+
+    # and render the template
+    return render(request, 'things/edit_thing.html', {
+        'thing': thing,
+        'form': form,
+    })
+
+
 def create_thing(request):
     form_class = ThingForm
+
+    # if we're coming from a submitted form, do this
     if request.method == 'POST':
+        # grab the data from the submitted form and apply to the form
         form = form_class(request.POST)
+
         if form.is_valid():
+            # create an instance but do not save yet
             thing = form.save(commit=False)
+
+            # set the additional details
             thing.user = request.user
             thing.slug = slugify(thing.name)
+
+            # save the object
             thing.save()
+
+            # redirect to our newly created thing
             return redirect('thing_detail', slug=thing.slug)
+
+    # otherwise just create the form
     else:
         form = form_class()
-    return render(request, 'things/create_thing.html', {'form':form,})
+
+    return render(request, 'things/create_thing.html', {
+        'form': form,
+    })
+
+
--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -15,23 +15,24 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth.views import password_reset, password_reset_done, password_reset_confirm, password_reset_complete
 from django.views.generic import TemplateView
-from django.contrib.auth.views import (
-    password_reset,
-    password_reset_done,
-    password_reset_confirm,
-    password_reset_complete
-)
 
+from collection.backends import MyRegistrationView
 from collection import views
+
 
 urlpatterns = [
     url(r'^$', views.index, name='home'),
-    url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
-    url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
-    url(r'^things/(?P<slug>[-\w]+)/$', views.thing_detail, name='thing_detail'),
-    url(r'^things/(?P<slug>[-\w]+)/edit/$',views.edit_thing, name='edit_thing'),
-# password reset urls    
+    url(r'^about/$',
+        TemplateView.as_view(template_name='about.html'), name='about'),
+    url(r'^contact/$',
+        TemplateView.as_view(template_name='contact.html'), name='contact'),
+    url(r'^things/(?P<slug>[-\w]+)/$', 
+        views.thing_detail, name='thing_detail'),
+    url(r'^things/(?P<slug>[-\w]+)/edit/$', 
+        views.edit_thing, name='edit_thing'),
+
     url(r'^accounts/password/reset/$', password_reset, 
         {'template_name': 'registration/password_reset_form.html'}, 
         name="password_reset"),
@@ -46,7 +47,13 @@ urlpatterns = [
     url(r'^accounts/password/done/$', password_reset_complete,
         {'template_name': 'registration/password_reset_complete.html'},
         name="password_reset_complete"),
+
+    url(r'^accounts/register/$', 
+        MyRegistrationView.as_view(), name='registration_register'),
+    url(r'^accounts/create_thing/$', 
+        views.create_thing, name='registration_create_thing'),
+
     url(r'^accounts/', include('registration.backends.simple.urls')),
-# admin urls    
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', include(admin.site.urls)),
 ]
+
